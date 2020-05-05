e795877b9e1eabb0c11dccb00cc87b1fc0b1ddaf
Chris Shealy <chriskshealy@gmail.com>
2020-05-05 16:36:50 -0600

    updated README; added env vars

--- README.md
+++ README.md
@@ -1,9 +1,20 @@
 # Sync Sandbox #
 
+## Compiling Protocol Buffers ##
+
+Protocol buffers can be found in the `proto/` directory, and contain definitions for gRPC calls. 
+
+Each field should contain detailed documentation for future feature work.
+
+Any changes to `*.proto` files will require re-compilation -- luckily this can easily be done by running:
+
+```shell script
+sh start.sh --compile
+```
+
 ## TODO ##
 
-- Docker-compose
-- Start script
+- Docker-compose (API server)
 - Swagger docs
 - Setup API server
 - Metrics
--- main.go
+++ main.go
@@ -1,16 +1,40 @@
 package main
 
 import (
+	"github.com/cshealy/sync-sandbox/data/spotify"
+	"github.com/kelseyhightower/envconfig"
 	log "github.com/sirupsen/logrus"
-	"sync-sandbox/data/spotify"
 )
 
+// 12 factor config for establishing connections and other various attributes with env vars
+type Config struct {
+	ServerPort int `required:"true" default:"50051" split_words:"true"` // SERVER_PORT
+}
+
 func main() {
-	spotifyAuth, err := spotify.NewSpotifyDAO()
+
+	// process environment variables for config
+	var config Config
+	err := envconfig.Process("sync-sandbox", &config)
+
+	// check for any errors while parsing environment variables
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal(err.Error())
 	}
 
+	log.WithFields(log.Fields{
+		"config": config,
+	}).Info("initialized config")
+
+	// create dao for spotify
+	spotifyAuth, err := spotify.NewSpotifyDAO()
+
+	// check for any errors while fetching the spotify bearer token
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	// confirm spotify dao is generated
 	log.WithFields(log.Fields{
 		"spotify_auth": spotifyAuth,
 	}).Info("fetched auth token")
